0189a6ed348fbc185ac9586bfd1ec2322e30a89d
ITACHIG18 <warinevi@gmail.com>
2026-09-20 18:25:57 +0300

    Add gunicorn to dependencies

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@
 import React, { useEffect, useMemo, useState } from "react";
 
 const API_URL =
-  process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api";
-
+  "https://pathwheeler-backend.onrender.com/api";
 
 // ============================================================
 // API HELPER
